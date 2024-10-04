995ae22ab90fd8c5f4fbf1dacaec7d9f64ec3466
tomaszdanielewski <tomasz.danielewski@gazeta.pl>
2024-10-04 16:13:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write("The name of your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
